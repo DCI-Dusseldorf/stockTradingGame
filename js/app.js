0d05c31a394c70a2ea01361b7cd67341ebf4fefd
Anh Tung Bui <anhtung.bui@gmail.com>
2020-09-16 21:35:22 +0200

    Initial UI Design / Base template

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// THE DEFAULT CHART IS JUST FOR DEMO PURPOSED
+// START EDIT YOUR JS FILE FROM HERE
+
+Highcharts.getJSON(
+  'https://demo-live-data.highcharts.com/aapl-c.json',
+  function (data) {
+    // Create the chart
+    Highcharts.stockChart('chart-container', {
+      chart: {
+        type: 'spline',
+      },
+      rangeSelector: {
+        selected: 1,
+      },
+
+      title: {
+        text: 'AAPL Stock Price',
+      },
+
+      series: [
+        {
+          name: 'AAPL',
+          data: data,
+          tooltip: {
+            valueDecimals: 2,
+          },
+        },
+      ],
+    });
+  }
+);
