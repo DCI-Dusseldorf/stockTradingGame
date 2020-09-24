ce483466abf5b8cde64b98667070ac0463771a9a
Nisha <nishashukla1987@gmail.com>
2020-09-24 18:12:40 +0200

    included Trend,Like btn display and modified date

--- js/app.js
+++ js/app.js
@@ -4,32 +4,21 @@ import Chart from "./Chart.js";
 import Search from "./Search.js";
 
 let service = new StockProxy(new FinnHubService());
-// if we want to use web service directly -> let service = new FinnHubService();
-
-let timeFrom = Date.parse("2020-01-16T03:24:00") / 1000;
-let timeTo = Date.parse("2020-11-16T03:24:00") / 1000;
-// AAPL,IBM,EBAY,GOOG
-let companyName = "GOOG";
 let chart = new Chart();
-service.getData(companyName, timeFrom, timeTo).then((data) => {
-  chart.renderChart(data);
-});
 
 window.addEventListener(
   "hashchange",
   function () {
-    console.log(location.hash.slice(1));
-
-    let timeFrom = Date.parse("2020-01-16T03:24:00") / 1000;
-    let timeTo = Date.parse("2020-11-16T03:24:00") / 1000;
-    // AAPL,IBM,EBAY,GOOG
+    const timeFrom = Date.parse("2020-01-01T00:00:00") / 1000;
+    const timeTo = new Date().getTime();
     let companyName = location.hash.slice(1);
-    let chart = new Chart();
     service.getData(companyName, timeFrom, timeTo).then((data) => {
       chart.renderChart(data);
     });
     $("#buyBtn").removeClass("d-none");
     $("#sellBtn").removeClass("d-none");
+    $("#trendsBtn").removeClass("d-none");
+    $("#favoriteBtn").removeClass("d-none");
   },
   false
 );
