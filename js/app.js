043df44ceaa848c78b917c72c551787503bf3c09
Madhumitha <mithalns@gmail.com>
2020-09-28 15:10:19 +0200

    added controllers from portfolio to app.js and removed buyandsell class and made it in portfolio class

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ import FinnHubService from "./api/FinnHubService.js";
 import StockProxy from "./proxy/StockProxy.js";
 import Chart from "./Chart.js";
 import Search from "./Search.js";
+import { StockData, Portfolio } from "./portfolio.js";
 
 let search = new Search();
+let PORTFOLIO = new Portfolio();
+let stock = new StockData();
+
 const searchForm = document.getElementById("searchForm");
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -33,3 +37,70 @@ window.addEventListener(
   },
   false
 );
+
+// Portfolio buy, sell and display
+
+const symbol = document.getElementById("stockChoice");
+const quantity = document.getElementById("quantity");
+const btnExecute = document.getElementById("executeOrder");
+
+//Buy order execution
+const buyBtn = document.getElementById("buyBtn");
+buyBtn.addEventListener("click", (e) => {
+  symbol.value = location.hash.slice(1);
+  btnExecute.addEventListener("click", (e) => {
+    e.preventDefault();
+    //const exec = new BuyOrSell();
+    PORTFOLIO.executeBuy(symbol.value, quantity.value);
+    display();
+  });
+});
+
+//Sell order execution
+const sellBtn = document.getElementById("sellBtn");
+sellBtn.addEventListener("click", (e) => {
+  symbol.value = location.hash.slice(1);
+  btnExecute.addEventListener("click", (e) => {
+    e.preventDefault();
+    //const exec = new BuyOrSell();
+    PORTFOLIO.executeSell(symbol.value, quantity.value);
+    display();
+  });
+});
+
+const PORTFDISPLAY = document.querySelectorAll("#balance");
+const CASHDISPLAY = document.querySelectorAll("#cash");
+
+function display() {
+  console.log(PORTFOLIO.computePortfValue().toFixed(2));
+
+  PORTFDISPLAY.forEach((element) => {
+    element.innerHTML = PORTFOLIO.computePortfValue().toFixed(2);
+  });
+  CASHDISPLAY.forEach((element) => {
+    element.innerHTML = JSON.parse(PORTFOLIO.retrieveCash()).toFixed(2);
+  });
+  let myStocks = PORTFOLIO.computeQuantity();
+  let boughtStocks = "";
+
+  Object.keys(myStocks).forEach(async function (key) {
+    const stockValue = myStocks[key] * (await stock.getCurrentPrice(key));
+    //console.log(key, myStocks[key]);
+
+    boughtStocks += `<div
+  class="a bg-white bg-hover-gradient-blue shadow roundy px-4 py-3 d-flex align-items-center justify-content-between mb-4"
+  >
+  <div class="2 flex-grow-1 d-flex align-items-center">
+    <div class="3 text">
+      <h6 class="4 mb-0">${key}</h6>
+      <span class="5 text-gray">${myStocks[key]}Shares</span>
+    </div>
+  </div>
+  <div class="6 text-green font-weight-bold">
+      ${stockValue.toFixed(2)}
+  </div>
+  </div>`;
+    $("#portfolioItems").html(boughtStocks);
+  });
+}
+display();
